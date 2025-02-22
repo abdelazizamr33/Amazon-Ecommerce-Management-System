c4e5d5aad784efd704b1fd98de89b70a148062f1
abdelaziz amr <130320084+abdelazizamr33@users.noreply.github.com>
2025-02-22 23:05:44 +0200

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonManagmentSystem.Models.Users
+{
+    internal class Customer : User
+    {
+        public List<Product> Cart { get; set; }
+        public List<Product> PurchasedProducts { get; set; }
+        public double MoneySpent { get; set; }
+        public void ViewCart(Customer customer)
+        {
+            foreach (var product in customer.Cart)
+            {
+                product.IsInCart = true;
+                Console.WriteLine(product);
+            }
+        }
+        public void AddToCart(List<Product> Products,int AddedProductId)
+        {
+            Product product = Products.FirstOrDefault(p => p.ProductID == AddedProductId);
+            Cart.Add(product);
+            Console.WriteLine("Added To Cart ✅");
+        }
+
+        public void RemoveFromCart(Product product)
+        {
+            Cart.Remove(product);
+        }
+        public Customer(string email, string name, string phonenum, string visanum, string pass)
+    : base(email, name, phonenum, visanum, pass)
+
+        {
+            Cart = new List<Product>();
+            PurchasedProducts = new List<Product>();
+            MoneySpent = 0;
+        }
+        public Customer()
+        {
+        }
+        
+        static Customer()
+        {
+
+        }
+
+        public void PurchaseProduct(Product product)
+        {
+            PurchasedProducts.Add(product);
+
+        }
+
+    }
+}
---
+++ Models/Merchant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonManagmentSystem.Models.Users
+{
+    internal class Merchant : User
+    {
+        
+        public List<Product> ProductSelling { get; set; }
+        public List<Product> ProductSold { get; set; }
+        public double Profit { get; set; }
+        public void ListProduct()
+        {
+
+        }
+        public void UnListProduct()
+        {
+
+        }
+        public Merchant()
+        {
+            ProductSelling = new List<Product>();
+            ProductSold = new List<Product>();
+            Profit = 0;
+        }
+        public Merchant(string email, string name, string phonenum, string visanum, string pass)
+    : base(email, name, phonenum, visanum, pass)
+
+        {
+            ProductSelling = new List<Product>();
+            ProductSold = new List<Product>();
+            Profit = 0;
+            ProductSelling.Add(new Product(1, "Phone", "IPHONE 16 pro", "256 Gb", 120000, 4));
+            ProductSelling.Add(new Product(2, "TV", "LG", "50 inch", 13000, 2));
+            ProductSold.Add(new Product(1, "Laptop", "Hp victus", "256 Gb", 120000, 4));
+            ProductSold.Add(new Product(2, "Home Appliances", "Fresh Blender", "make good foof", 900, 5));
+
+        }
+    }
+}
